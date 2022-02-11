b75a01bdf9f108dfc615a0f7fbec1f6f3e974705
aktan <aktansuper2005@email.ru>
2022-02-12 01:42:53 +0600

    example

--- example.py
+++ example.py
@@ -1,30 +1,20 @@
 
 a = {
-'game':" gta"
+ 'game':" gta"
 }
 a.update({'cinema':"hrre a"})
+ print(a) 
+a.update({'trainings' : "push ups"}) 
 print(a)
-a.update({'trainings' : "push ups"})
-print(a)
-a.update({'cars': "Mersedes"})
-print(a)
-a.pop("game")
-print(a)
+ a.update({'cars': "Mersedes"})
+ print(a)
+ a.pop("game")
+ print(a)
 
-a = {
-'cow',
-'dog',
-'fox',
-'cat'
+a = { 'cow', 'dog', 'fox', 'cat'
 }
-b = {
-'horse',
-'fox',
-'cat',
-'dog'
-}
-a.difference(b)
-print(a)
+b = { 'horse', 'fox', 'cat', 'dog'}
+a.difference(b) print(a)
 
 what = input(" что делаем + , - , * , / ")
 a = float(input(" введите первое число: ") )
@@ -46,5 +36,39 @@ else:
 
 proffesion = {'Jacob':driverl
 
-numbers = {21,21,21,23,12}
+
+numbers = {'21':2,'21':3,'21':-23}
+numbers.add('babl kwas')
 print(numbers)
+
+
+print('добро пожаловать на наше кафе')
+
+oficiant = str(input('вам дать меню?:'))
+
+if oficiant == ('да'):
+	print('что тогда закажите')
+elif oficiant == ('нет'):
+	print('')
+
+menu = {
+'Пицца': ['ПЕПЕРОНИ','ОХОТНИЧЬЯ','ГРИБНАЯ','С ОЛИВКАМИ']
+}
+print(menu)
+menu.update({'Спагетти' : 1})
+print(menu)
+west = str(input('что выберите'))
+if west == ('ПЕПЕРОНИ'):
+	print('отличный выбор')
+elif west == ('ОХОТНИЧЬЯ'):
+	print('отличный выбор')
+print(west + 'будет готова через 6 минут')
+drink = str(input('закажите что нибудь попить?:'))
+if drink == ('да'):
+	print('хорошо что выберите')
+elif drink == ('нет'):
+
+	print('ладно , принесу тогда вам чай индийский')
+drinks={
+'juices':[Fanta, Coca-Cola , Sprite , Pepsi]
+}
