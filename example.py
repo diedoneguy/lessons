c773a210191112c510285581263dcd24deeb73ea
aktan <aktansuper2005@email.ru>
2022-02-11 20:32:26 +0600

    example

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-
+'''
 a = {'game':" gta"}
 a.update({'cinema':"hrre a"})
 print(a)
@@ -7,4 +7,9 @@ print(a)
 a.update({'cars': "Mersedes"})
 print(a)
 a.pop("game")
+print(a)
+'''
+a = {'cow','dog','fox','cat'}
+b = {'horse','fox','cat','dog'}
+a.difference(b)
 print(a)
